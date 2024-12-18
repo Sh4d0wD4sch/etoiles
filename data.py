9c4fca948619a2f7fdf6704cc80ff911c9f9e670
Sh4d0wD4sch <thomas.mercier@spacepin-consulting.com>
2024-12-18 16:27:45 +0100

    aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1SR1ZCdEt0R2Zkcw==

---
+++ data.py
@@ -0,0 +1,9 @@
+p = {
+    "Et ce Destin qui nous conduit": [0x2E],
+    "Ainsi vous plaît, Etoiles": [0x31, 0x38, 0x35],
+    "Je vous salue, heureuses flammes": [0x2E],
+    "Que vous pendîtes à nos trames": [0x31, 0x30, 0x33],
+    "Etoiles, filles de la Nuit": [0x31, 0x34, 0x33],
+    "Vous dévidez vos cours": [0x32, 0x32, 0x39],
+    "Tandis que tous les jours": [0x2E]
+}
